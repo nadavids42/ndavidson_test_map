a311b4ad2f5614381bc730d1a1daca91c997a687
nadavids42 <ndavidson42@gmail.com>
2025-04-29 09:24:42 -0400

    Update load_data.js

--- js/load_data.js
+++ js/load_data.js
@@ -2,6 +2,6 @@ export function loadData() {
   return Promise.all([
     d3.json("/data/SchoolDistricts_poly.geojson"),
     d3.csv("/data/Cleaned_data.csv"),
-    d3.json("/data/Massachusetts_districts.geojson")
+    // d3.json("/data/Massachusetts_districts.geojson")
   ]);
 }
